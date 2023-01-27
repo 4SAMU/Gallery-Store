bd0bace0c9dd7af3eb8524fa3949aa65565507e6
4SAMU <samuelnzomo4@gmail.com>
2023-01-27 18:20:15 +0300

    update on login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -8,16 +8,22 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div className="LogingPage">
-      <div className="login_header">LOGIN</div>
       <img className="bg_login" src={BabyThor} alt="" />
-      <input className="login_input" type={"text"} placeholder="name" />
-      <input className="login_input" type={"email"} placeholder="email" />
-      <input className="login_input" type={"password"} placeholder="password" />
-      <button className="signupbtn">SIGN IN</button>
-      <p className="donthaveAcc">don’t have an account?</p>
-      <Link to={"/Signup"}>
-        <p className="donthaveAcc_p">Signup</p>
-      </Link>
+      <div className="login_container">
+        <div className="login_header">LOGIN</div>
+        <input className="login_input" type={"text"} placeholder="name" />
+        <input className="login_input" type={"email"} placeholder="email" />
+        <input
+          className="login_input"
+          type={"password"}
+          placeholder="password"
+        />
+        <button className="signupbtn">SIGN IN</button>
+        <p className="donthaveAcc">don’t have an account?</p>
+        <Link to={"/Signup"}>
+          <p className="donthaveAcc_p">Signup</p>
+        </Link>
+      </div>
     </div>
   );
 };
