ff6de0697458fd75a1a5ce2f920eada41b51c5e1
4SAMU <samuelnzomo4@gmail.com>
2023-01-27 17:38:45 +0300

    update on login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,17 @@
 /** @format */
 
 import React from "react";
-import "./Login.css"
+import "./Login.css";
 import BabyThor from "../../assets/thorbaby.jpeg";
 
 const Login = () => {
   return (
     <div className="LogingPage">
-      <img className="bg_login" src={BabyThor} alt="" />
+      <div className="login_header">LOGIN</div>
+      <img className="bg_login" src={BabyThor} alt=""/>
+      <input className="login_input" type={"text"} placeholder="name" />
+      <input className="login_input" type={"email"} placeholder="email" />
+      <input className="login_input" type={"password"} placeholder="password" />
     </div>
   );
 };
