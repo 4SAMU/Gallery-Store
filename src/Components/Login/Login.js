0c983ed4bdc73e2bdd29977f586e68606e77933d
4SAMU <samuelnzomo4@gmail.com>
2023-01-27 19:08:14 +0300

    update on signup

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -10,14 +10,13 @@ const Login = () => {
       <img className="bg_login" src={BabyThor} alt="" />
       <div className="login_container">
         <div className="login_header">LOGIN</div>
-        <input className="login_input" type={"text"} placeholder="name" />
         <input className="login_input" type={"email"} placeholder="email" />
         <input
           className="login_input"
           type={"password"}
           placeholder="password"
         />
-        <button className="signupbtn">SIGN IN</button>
+        <button className="signinbtn">SIGN IN</button>
         <p className="donthaveAcc">don’t have an account?</p>
         <Link to={"/Signup"}>
           <p className="donthaveAcc_p">Signup</p>
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,13 +1,29 @@
 /** @format */
 
 import React from "react";
+import { Link } from "react-router-dom";
 import BabyThor from "../../assets/thor.png";
-
+import "./Signup.css";
 
 const Signup = () => {
   return (
     <div className="Page">
       <img className="bg_login" src={BabyThor} alt="" />
+      <div className="signup_container">
+        <div className="signup_header">signup</div>
+        <input className="signup_input" type={"text"} placeholder="name" />
+        <input className="signup_input" type={"email"} placeholder="email" />
+        <input
+          className="signup_input"
+          type={"password"}
+          placeholder="password"
+        />
+        <button className="signupbtn">SIGN UP</button>
+        <p className="alreadyhaveAcc">don’t have an account?</p>
+        <Link to={"/Signup"}>
+          <p className="alreadyhaveAcc_p">Signup</p>
+        </Link>
+      </div>
     </div>
   );
 };
