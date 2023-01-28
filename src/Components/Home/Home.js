4eece5fb61fa726399c359b68de77cbfaa58298d
4SAMU <samuelnzomo4@gmail.com>
2023-01-28 12:10:58 +0300

    update on home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,11 @@
 
 import React from "react";
 import "./Home.css";
+import Thor from "../../assets/thorbaby.jpeg";
+import likeBtn from "../../assets/likeBtn.svg";
+import downloadBtn from "../../assets/downloadBtn.svg";
+import samu from "../../assets/samu.jpeg";
+import Navbar from "../Navabar/Navbar";
 
 const Home = () => {
   return (
@@ -9,30 +14,47 @@ const Home = () => {
       <div className="Home_header_container">
         <p className="Home_header">Hello, Samuel</p>
         <div className="userImage">
-          <img src={""} alt="" />
+          <img src={samu} alt="" className="userImage" />
         </div>
       </div>
       <div className="Story_status">
         <div className="Story_status_pics">
-          <img src="" alt="" />
+          <img src={Thor} alt="" className="status_pics" />
         </div>
         <div className="Story_status_pics">
-          <img src="" alt="" />
+          <img src={Thor} alt="" className="status_pics" />
         </div>
         <div className="Story_status_pics">
-          <img src="" alt="" />
+          <img src={Thor} alt="" className="status_pics" />
         </div>
         <div className="Story_status_pics">
-          <img src="" alt="" />
+          <img src={Thor} alt="" className="status_pics" />
         </div>
       </div>
 
       <div className="many_pics_container">
-        <div className="pic_card"></div>
-        <div className="pic_card"></div>
-        <div className="pic_card"></div>
-        <div className="pic_card"></div>
+        <div className="pic_card">
+          <img src={Thor} alt="" className="myPic" />
+          <img src={likeBtn} alt="" className="likeBtn" />
+          <img src={downloadBtn} alt="" className="downloadBtn" />
+          <p className="caption">Baby thor</p>
+        </div>
+
+        <div className="pic_card">
+          <img src={Thor} alt="" className="myPic" />
+          <img src={likeBtn} alt="" className="likeBtn" />
+          <img src={downloadBtn} alt="" className="downloadBtn" />
+          <p className="caption">Baby thor</p>
+        </div>
+
+        <div className="pic_card">
+          <img src={Thor} alt="" className="myPic" />
+          <img src={likeBtn} alt="" className="likeBtn" />
+          <img src={downloadBtn} alt="" className="downloadBtn" />
+          <p className="caption">Baby thor</p>
+        </div>
       </div>
+      <Navbar />
     </div>
   );
 };
---
+++ src/Components/Navabar/Navbar.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return <div className="Navbar"></div>;
+};
+
+export default Navbar;
