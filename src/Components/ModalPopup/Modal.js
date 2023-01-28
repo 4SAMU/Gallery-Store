79196cbd7eae30b56e49827a9225e864e09d63fa
4SAMU <samuelnzomo4@gmail.com>
2023-01-28 14:10:50 +0300

    model popup userimage click

--- src/Components/ModalPopup/Modal.js
+++ src/Components/ModalPopup/Modal.js
@@ -1,13 +1,16 @@
 /** @format */
 
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Modal.css";
 
 const Modal = ({ isOpen }) => {
   return (
     <div className={`ModalContainer ${isOpen ? "open" : "close"}`}>
       <p className="ModalContainer_header">Update your profile</p>
-      <button className="proceedBtn">Update Profile</button>
+      <Link to={"/Profile"}>
+        <button className="proceedBtn">Update Profile</button>
+      </Link>
     </div>
   );
 };
