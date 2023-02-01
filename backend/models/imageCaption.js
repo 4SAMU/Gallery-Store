c62505c883ccec2fdf1fef6fad75495dce69b3d4
4SAMU <samuelnzomo4@gmail.com>
2023-02-01 19:16:19 +0300

    update on api endpoint

--- backend/models/imageCaption.js
+++ backend/models/imageCaption.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const imageCaption = new mongoose.Schema({
   caption: { type: String, required: true },
-  image: { type: String, required: false },
+  image: { type: String, required: true },
 });
 
 module.exports = mongoose.model("imageCaption", imageCaption);
