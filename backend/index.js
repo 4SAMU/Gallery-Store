8bebebf6aad32f028a6a6ed3a72a3934a1792a17
4SAMU <samuelnzomo4@gmail.com>
2023-02-02 13:44:36 +0300

    update on delete router

--- backend/index.js
+++ backend/index.js
@@ -227,6 +227,40 @@ app.get("/getFiles", (req, res) => {
   });
 });
 
+app.delete("/deleteCaptionData/:id", (req, res) => {
+   const id = req.params.id;
+
+   imageCaption.findByIdAndDelete(id, (err, deletedFile) => {
+     if (err)
+       return res.status(500).json({ status: "error", message: err.message });
+     if (!deletedFile)
+       return res
+         .status(404)
+         .json({ status: "error", message: "File not found" });
+
+     res
+       .status(200)
+       .json({ status: "ok", message: "File deleted successfully" });
+   });
+});
+
+app.delete("/deleteImageUpload/:id", (req, res) => {
+  const id = req.params.id;
+
+  imageUpload.findByIdAndDelete(id, (err, deletedFile) => {
+    if (err)
+      return res.status(500).json({ status: "error", message: err.message });
+    if (!deletedFile)
+      return res
+        .status(404)
+        .json({ status: "error", message: "File not found" });
+
+    res
+      .status(200)
+      .json({ status: "ok", message: "File deleted successfully" });
+  });
+});
+
 //Server listening to routes
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}...`);
