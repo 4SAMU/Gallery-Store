1d6854cd99c25f89aa2ab06ba9001fa1a2fb5b9e
4SAMU <samuelnzomo4@gmail.com>
2023-02-04 11:15:48 +0300

    loader in login page

--- client_app/src/Components/Signup/Signup.js
+++ client_app/src/Components/Signup/Signup.js
@@ -5,40 +5,57 @@ import { Link } from "react-router-dom";
 import BabyThor from "../../assets/thor.png";
 import Google from "../../assets/google.png";
 import "./Signup.css";
+import TransclucentBg from "../LoaderTransclucentBg/TransclucentBg";
+import { toast } from "react-toastify";
 
 const Signup = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [busy, setBusy] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   async function registerUser(event) {
-    event.preventDefault();
+    if (!name && !email && !password) {
+      setIsModalOpen(false);
+      setBusy(false);
+      toast.warn("all fields must be filled");
+    } else {
+      setIsModalOpen(true);
+      setBusy(true);
+      event.preventDefault();
 
-    const response = await fetch(
-      "https://gallery-store-api.vercel.app/register",
-      {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({
-          name,
-          email,
-          password,
-        }),
-      }
-    );
+      const response = await fetch(
+        "https://gallery-store-api.vercel.app/register",
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            email,
+            password,
+          }),
+        }
+      );
 
-    const data = await response.json();
-    console.log(data);
-
-    if (data.status === "ok") {
-      window.location.replace("/Login");
+      const data = await response.json();
       console.log(data);
 
-      // alert("status ok");
-    } else if (data.status === "error") {
-      alert(data.error);
+      if (data.status === "ok") {
+        toast.success("account created successfully");
+        setTimeout(function () {
+          // code to be executed after 3 seconds
+          window.location.href = "/Login";
+        }, 3000);
+
+        // alert("status ok");
+      } else if (data.status === "error") {
+        setIsModalOpen(false);
+        setBusy(false);
+        toast.warn(data.error);
+      }
     }
   }
   return (
@@ -71,7 +88,7 @@ const Signup = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
         <button className="signupbtn" onClick={registerUser}>
-          SIGN UP
+          {busy ? "loading..." : " SIGN UP"}
         </button>
         <button className="signupbtn">
           sign up with
@@ -82,6 +99,7 @@ const Signup = () => {
           <p className="alreadyhaveAcc_p">Sign in</p>
         </Link>
       </div>
+      <TransclucentBg isOpen={isModalOpen} />
     </div>
   );
 };
