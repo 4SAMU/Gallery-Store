883e82d7adb901c882c579534a4ff28da17a3bb1
4SAMU <samuelnzomo4@gmail.com>
2023-02-10 14:46:37 +0300

    converted the app to a PWA

---
+++ client_app/public/sw.js
@@ -0,0 +1,13 @@
+// In a separate JavaScript file, e.g. sw.js
+
+self.addEventListener("install", function (event) {
+  console.log("[Service Worker] Installed");
+});
+
+self.addEventListener("activate", function (event) {
+  console.log("[Service Worker] Activated");
+});
+
+self.addEventListener("fetch", function (event) {
+  console.log("[Service Worker] Fetching", event.request.url);
+});
