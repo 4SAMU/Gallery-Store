bef6feb7f671fb6a9c361622ef1edea8901a057a
4SAMU <samuelnzomo4@gmail.com>
2023-02-14 16:21:27 +0300

    message api link

--- Messaging_api/index.js
+++ Messaging_api/index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000" || "https://gallery-store.vercel.app",
+    origin: "https://gallery-store.vercel.app",
     methods: ["GET", "POST"],
   },
 });
