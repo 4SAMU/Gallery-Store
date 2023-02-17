63cf77b5eee0169918383404e545478f4178b065
4SAMU <samuelnzomo4@gmail.com>
2023-02-17 19:27:57 +0300

    update

--- client_app/src/utils/MakeClickable.js
+++ client_app/src/utils/MakeClickable.js
@@ -2,7 +2,9 @@
 
 export default function makeTextClickable(text) {
   const urlRegex = /(https?:\/\/[^\s]+)/g;
-  const phoneRegex = /(\+?\d{1,3}[-.\s]?)?\d{3}[-.\s]?\d{3}[-.\s]?\d{4}/; // Match phone numbers with an optional country code and with spaces, dashes, or dots between digits
+  const phoneRegex =
+    /(\+?\d{1,3}[-.\s]?)?\d{3}[-.\s]?\d{3}[-.\s]?\d{4}/ ||
+    /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g; // Match phone numbers with an optional country code and with spaces, dashes, or dots between digits
   const emailRegex = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i;
   const parts = text.split(
     /(\b(?:https?:\/\/|mailto:)?\S+\b|\+?\d{1,3}[-.\s]?\d{3}[-.\s]?\d{3}[-.\s]?\d{4}\b|\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b)/gi
